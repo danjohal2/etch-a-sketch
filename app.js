329acc1ccffb2ba405ba0bec0a252acb5fb2b2c1
Dan Johal <dan.johal2@gmail.com>
2021-01-18 14:57:51 -0800

    working reset button

--- app.js
+++ app.js
@@ -1,37 +1,51 @@
+
+
 let createGrid = (number = 16) => {
-for(i = 0; i < number ** 2; i++) {
-	const container = document.querySelector('.container');
-	container.style.display = 'grid';
-  container.style.gridTemplateColumns = `repeat(${number}, 1fr)`;
-  container.style.gridTemplateRows = `repeat(${number}, 1fr)`;
-  const square = document.createElement('div');
-  square.classList.add('square');
-  container.appendChild(square);
+  for(i = 0; i < number ** 2; i++) {
+    const container = document.querySelector('.container');
+    container.style.display = 'grid';
+    container.style.gridTemplateColumns = `repeat(${number}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${number}, 1fr)`;
+    const square = document.createElement('div');
+    square.classList.add('square');
+    container.appendChild(square);
+    
+    };
+  
+  
+    
+    let hover = () => {
+      let squares = document.querySelectorAll('.square');
+      squares.forEach(square => square.addEventListener('mouseover', ()=> {
+          square.style.backgroundColor = 'black';
+      }));   
   };
-  hover();
-};
-   
-let hover = () => {
-    let squares = document.querySelectorAll('.square');
-    squares.forEach(square => square.addEventListener('mouseover', ()=> {
-        square.style.backgroundColor = 'black'
-    }))
-}
-
+    hover();
   
-
-
-  /* let squares = document.querySelectorAll('.square');
-   squares.forEach(square => {
-  // on mouseover
-    square.addEventListener('mouseover', (e) => {
-      e.target.style.backgroundColor = 'rgb(0,0,0)';
+    let erase = () => {
+      let squares = document.querySelectorAll('.square');
+      squares.forEach(square => {
+        square.style.backgroundColor = 'white';
+      })
+    };
   
-    })
-  }) 
-  // this one ^ will lag if you have a large number of squares, do event bubbling/delegation instead
-};
-
-*/
-
-createGrid();
+  
+    const resetButton = document.querySelector('.reset');
+    resetButton.addEventListener('click', erase);
+    
+  };
+  
+    /* let squares = document.querySelectorAll('.square');
+     squares.forEach(square => {
+    // on mouseover
+      square.addEventListener('mouseover', (e) => {
+        e.target.style.backgroundColor = 'rgb(0,0,0)';
+    
+      })
+    }) 
+    // this one ^ will lag if you have a large number of squares, do event bubbling/delegation instead
+  };
+  
+  */
+  
+  createGrid();
