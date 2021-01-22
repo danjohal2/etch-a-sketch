ebe3475f7e901b6814a7b91691c55dba980f525a
Dan Johal <dan.johal2@gmail.com>
2021-01-22 00:07:10 -0800

    everything done except gradient(optional)

--- app.js
+++ app.js
@@ -11,28 +11,65 @@ let createGrid = (number = 16) => {
     container.appendChild(square);
     
     };
-  
-  
     
     let hover = () => {
       let squares = document.querySelectorAll('.square');
       squares.forEach(square => square.addEventListener('mouseover', ()=> {
           square.style.backgroundColor = 'black';
       }));   
-  };
-    hover();
+      }; hover(); 
+    
   
     let erase = () => {
       let squares = document.querySelectorAll('.square');
       squares.forEach(square => {
-        square.style.backgroundColor = 'white';
-      })
-    };
+        square.style.backgroundColor = "white";
+      }); 
+    }; 
   
   
     const resetButton = document.querySelector('.reset');
     resetButton.addEventListener('click', erase);
-    
+
+  //check the console, square isnt defined when using new grid button
+    const newgrid = document.querySelector('.newgrid'); //newgrid button
+    newgrid.addEventListener('click', function(){
+      let newnum = +prompt("Enter a number between 1 and 100");
+      if (newnum >= 1 && newnum <= 100) {
+        /* document.querySelector('div').removeChild(square) */;
+        document.querySelector('.container').innerHTML = '';
+        createGrid(newnum);
+        hover();
+      } else {
+       prompt("Invalid entry");
+      
+      }
+    })
+
+	function setColors() {
+    const colors = document.querySelectorAll('.square');
+      colors.forEach(grid => grid.addEventListener('mouseover', function() {
+        let red = Math.floor(Math.random()*255);
+        let green = Math.floor(Math.random()*255);
+        let blue = Math.floor(Math.random()*255);
+        color = `rgb(${red}, ${green}, ${blue})`;
+        grid.style.backgroundColor = color;
+          /* let squares = document.querySelectorAll('.square');
+          squares.forEach(square => {
+            square.style.backgroundColor = color;
+          }); */
+      }));
+  }
+  
+  
+
+  
+  let colorchooser = document.querySelector('.colorsOn');
+  colorchooser.addEventListener('click', setColors);   
+
+  let colorchooserdefault = document.querySelector('.colorsOff');
+  colorchooserdefault.addEventListener('click', hover);
+
   };
   
     /* let squares = document.querySelectorAll('.square');
@@ -49,3 +86,4 @@ let createGrid = (number = 16) => {
   */
   
   createGrid();
+ 
